a0c7623e901dbc449db25933fbce380eb62a4253
rmad17 <souravbasu17@gmail.com>
2016-09-19 20:37:11 +0530

    Deleted secret key

--- collab/settings.py
+++ collab/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^n0ontkbrggu**d@$@*r135sp*+podl1q9jkty^dy1n^qolv5p'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
